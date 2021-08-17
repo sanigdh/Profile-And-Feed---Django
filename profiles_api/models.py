f1540bd3d55559b536f8bf800c3da78eb2293375
Sanigdh Chandan <sanigdh@Sanigdhs-MacBook-Air.local>
2021-08-17 13:34:18 +0530

    enable django admin for user profile admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,27 +4,28 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 class UserProfileManager(BaseUserManager):
-    """Manager for user profile"""
+    """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """create a new user profile"""
+        """Create a new user profile"""
         if not email:
             raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
-        user = self.model(email=email,name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
-        user.save(using=self_db)
+        user.save(using=self._db)
 
-        return users
+        return user
 
-    def create_superuser(self,email,name,password):
-        """create super user"""
-        user = self.create_user(email,name,password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=seld_db)
+        user.save(using=self._db)
 
         return user
 
@@ -39,16 +40,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """"Retrieve full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """"Retrieve full name of user"""
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """"return string representation of user"""
+        """Return string representation of user"""
         return self.email
