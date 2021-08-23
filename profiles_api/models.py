cb127851dbc93fa2c991221c85fb66cb5a465f3c
Sanigdh Chandan <sanigdh@Sanigdhs-MacBook-Air.local>
2021-08-23 17:09:22 +0530

    created migration for profile feed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 # Create your models here.
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
@@ -29,7 +30,6 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
@@ -53,3 +53,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of user"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile status updates"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        """Return the model as string"""
+        return self.status_text
